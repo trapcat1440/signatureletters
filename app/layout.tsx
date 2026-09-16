8051556e33b232fb410a1c5a415fb351b73d3028
trapcat1440 <lesterfigueroa90@gmail.com>
2026-09-15 22:47:41 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'SIGNATURELETTERSBLOG'
+  title: "SIGNATURELETTERSBLOG"
   description: "Finding quick details to tell  | Rights Reserved.",
   generator: 'v0.app',
   icons: {
