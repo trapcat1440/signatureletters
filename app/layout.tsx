4c32e065795eaeaa45b3dfbf4f56c7ded875aeda
trapcat1440 <lesterfigueroa90@gmail.com>
2026-09-15 22:28:29 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Signature & Letter SS | Blog Media',
-  description: 'Fashion, culture, brands, and digital life — Rights Reserved.',
+  title: 'SIGNATURELETTERSBLOG'
+  description: 'Finding quick details to tell — Rights Reserved.',
   generator: 'v0.app',
   icons: {
     icon: [
